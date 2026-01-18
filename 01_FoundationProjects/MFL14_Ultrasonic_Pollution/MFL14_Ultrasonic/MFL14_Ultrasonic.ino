98cfb56b1afc1bb936e7ea01cf05bfdbf0c7405f
mohammed-fakruddin <info@zas-robotics.com>
2026-01-18 00:13:19 +0000

    updated links

---
+++ 01_FoundationProjects/MFL14_Ultrasonic_Pollution/MFL14_Ultrasonic/MFL14_Ultrasonic.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Ultrasonic pins
+const int trigPin = 7;
+const int echoPin = 10;
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+
+  Serial.begin(9600);
+    // Initialize OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+}
+
+void loop() {
+  float distance = getDistance();
+
+  // Print both distance and MQ135 values
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm | ");
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Dist: ");
+  display.print(distance);
+  display.println(" cm");
+  display.display();
+  delay(100);
+}
+
+float getDistance() {
+  // Send trigger pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read echo time
+  long duration = pulseIn(echoPin, HIGH);
+
+  // Convert to cm
+  float distance = duration * 0.0343 / 2.0;
+  return distance;
+}
